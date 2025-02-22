d2733fb6de98f9303c9b34121a7923ef763c4078
tsukimizake <shomasd@gmail.com>
2025-02-22 20:39:30 +0900

    optional fn_name_str arg to lisp_fn macro

--- tauri/lisp_macro/src/lib.rs
+++ tauri/lisp_macro/src/lib.rs
@@ -1,12 +1,44 @@
 use proc_macro::TokenStream;
 use quote::quote;
-use syn::{parse_macro_input, ItemFn};
+use syn::{parse_macro_input, ItemFn, LitStr, parse::Parse, parse::ParseStream, Token};
 
+struct LispFnArgs {
+    name: Option<String>,
+}
+
+impl Parse for LispFnArgs {
+    fn parse(input: ParseStream) -> syn::Result<Self> {
+        if input.is_empty() {
+            return Ok(LispFnArgs { name: None });
+        }
+        let name_lit: LitStr = input.parse()?;
+        Ok(LispFnArgs { name: Some(name_lit.value()) })
+    }
+}
+
+/// Registers a function as a Lisp primitive that will be available in the Lisp environment.
+///
+/// # Examples
+///
+/// ```
+/// // Using the function name as the Lisp name
+/// #[lisp_fn]
+/// fn add(args: &[Arc<Expr>], env: Arc<Mutex<Env>>) -> Result<Arc<Expr>, String> {
+///     // registers as "add"
+/// }
+///
+/// // Using a custom name for the Lisp function
+/// #[lisp_fn("+")]
+/// fn add_op(args: &[Arc<Expr>], env: Arc<Mutex<Env>>) -> Result<Arc<Expr>, String> {
+///     // registers as "+"
+/// }
+/// ```
 #[proc_macro_attribute]
-pub fn lisp_fn(_attr: TokenStream, item: TokenStream) -> TokenStream {
+pub fn lisp_fn(attr: TokenStream, item: TokenStream) -> TokenStream {
+    let args = parse_macro_input!(attr as LispFnArgs);
     let input = parse_macro_input!(item as ItemFn);
     let fn_name = &input.sig.ident;
-    let fn_name_str = fn_name.to_string();
+    let fn_name_str = args.name.unwrap_or_else(|| fn_name.to_string());
 
     let expanded = quote! {
         #input
